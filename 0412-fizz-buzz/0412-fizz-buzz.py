f7ab925f38b4995e3d0ca301427edfb2f472f22b
Akotet Y. Tesema <46914459+Akotet08@users.noreply.github.com>
2024-01-31 04:06:48 +0900

    Time: 36 ms (94.21%), Space: 17.8 MB (55.99%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -0,0 +1,19 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        lst = ['x']*n
+        for i in range(1, n+1):
+            div3 = i%3==0
+            div5 = i%5==0
+            if div3 and div5:
+                lst[i-1] = 'FizzBuzz'
+            elif div3:
+                lst[i-1] = 'Fizz'
+            elif div5:
+                lst[i-1] = 'Buzz'
+            else:
+                lst[i-1] = str(i)
+        
+        return lst
+                
+
+        
