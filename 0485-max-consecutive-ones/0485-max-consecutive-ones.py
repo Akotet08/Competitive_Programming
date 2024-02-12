cfec9db72582395c4df37346094caf904558a4da
Akotet Y. Tesema <46914459+Akotet08@users.noreply.github.com>
2024-02-12 15:14:47 +0900

    Time: 274 ms (48.29%), Space: 16.8 MB (81.15%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.py
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.py
@@ -3,18 +3,15 @@ class Solution:
         i = 0
         mx = nums[0]
         while i < len(nums):
+            j = i + 1
             if nums[i] == 1:
-                j = i + 1
                 while j < len(nums) and  nums[j] == 1:
                     j += 1
                 
                 if j - i + 1 > mx:
                     mx = j - i
-                    i = j
-                else:
-                    i += 1
-            else:
-                i += 1
+
+            i = j
         return mx 
                 
 
