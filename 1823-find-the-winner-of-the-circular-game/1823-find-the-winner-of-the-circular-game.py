8d12715376b03f859ebb38226ac36de9324380ce
Akotet Y. Tesema <46914459+Akotet08@users.noreply.github.com>
2024-02-25 23:35:29 +0900

    Time: 49 ms (40.07%), Space: 16.6 MB (63.61%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        lst = list(range(1, n+1))
+        idx = 0
+        while len(lst) > 1:
+            idx = (idx + k -1) % len(lst)
+            lst.remove(lst[idx])
+        
+        return lst[0]
+ 
+
+
+        
