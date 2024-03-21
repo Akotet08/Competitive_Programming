89dd3c8ef9e1af787c5c79191f3c0d1387d36821
Akotet Y. Tesema <46914459+Akotet08@users.noreply.github.com>
2024-03-21 11:57:25 +0900

    Time: 832 ms (5.84%), Space: 23.9 MB (87.37%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -9,11 +9,8 @@ class Solution:
             baskets[fruits[i]] += 1
             while len(baskets) > 2 and baskets[fruits[j]] > 0:
                 baskets[fruits[j]] -= 1
-
                 if baskets[fruits[j]] == 0: del baskets[fruits[j]]
-                j += 1
-            
-            print(baskets)
+                j += 1            
             count = max(count, i-j+1)
             i += 1
         return count
