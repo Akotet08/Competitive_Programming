2a8c856b8bec5905f5b09b692f18bc8fccf4fe42
Akotet Y. Tesema <46914459+Akotet08@users.noreply.github.com>
2024-03-27 00:08:20 +0900

    Time: 123 ms (59.5%), Space: 17.8 MB (29.58%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -4,16 +4,12 @@ class Solution:
         forward_sum = [0] * (n)
         forward_sum[0] = nums[0]
 
-        back_sum = [0] * (n)
-        back_sum[-1] = nums[-1]
+        total = sum(nums)
 
         for i in range(1, n):
             forward_sum[i] = forward_sum[i-1] + nums[i]
         
-        for j in range(n-2, -1, -1):
-            back_sum[j] = back_sum[j + 1] + nums[j]
-        
         for i in range(n):
-            if forward_sum[i] == back_sum[i]:
+            if (forward_sum[i] - nums[i]) == total-(forward_sum[i]):
                 return i
         return -1
